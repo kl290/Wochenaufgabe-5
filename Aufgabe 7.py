c0d7b20ad97ede022809dd12a53dfcb7511b9382
kl <kl@intentus.de>
2024-10-08 11:17:14 +0200

    Initial commit for this project

--- Aufgabe 7.py
+++ Aufgabe 7.py
@@ -1,1 +1,5 @@
-print("Multiplikationstabelle")
+print("Multiplikationstabelle ( kleines 1-mal-1")
+Zahl = int(input(" Bitte gib eine zhal ein:"))
+for i in range(1,11):
+    ergebnis = Zahl * i
+    print(f"{Zahl} * {i} = {ergebnis}")
