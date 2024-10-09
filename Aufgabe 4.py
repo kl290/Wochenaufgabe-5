281a7f80ccddeab5b32a0852deb429fbb7f00852
kl <kl@intentus.de>
2024-10-09 14:11:10 +0200

    Verbesserung Aufgabe 4 -Prompt Gestaltung -Programm aktzeptiert auch Kommazahlen -Korrektur der Eingabe ermöglicht

--- Aufgabe 4.py
+++ Aufgabe 4.py
@@ -1,7 +1,12 @@
-Zahl = int(input("Bitte geben Sie eine Zahl ein:"))
-if Zahl == 0 :
-    print("Die eingegebene Zahl ist 0!")
-elif Zahl > 0:
-    print("Die eingegebene zahl ist positiv!")
-elif Zahl < 0:
-    print("Die eingegebene Zahl ist negativ!")
+while True:
+    try:
+        Zahl = int(input("Bitte geben Sie eine Zahl ein :"))
+        if Zahl == 0 :
+            print("Die eingegebene Zahl ist 0!")
+        elif Zahl > 0 :
+            print("Die eingegebene Zahl ist positiv!")
+        elif Zahl < 0 :
+            print("Die eingegebene Zahl ist negativ!")
+        break
+    except ValueError :
+        print("Ungültige Eingabe.Bitte eine Zahl eingeben!")
