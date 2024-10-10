4e30b175d33c5dbed8201858e804886219cddafb
kl <kl@intentus.de>
2024-10-10 13:22:38 +0200

    Verbesserung Aufgabe 7
    - Prompt Gestaltung
    - Korrektur der Eingabe ermöglicht (Meldung)
    - Kommunikation mit Nutzer verbessert
    - Rechtschreibung

--- Aufgabe 7.py
+++ Aufgabe 7.py
@@ -1,5 +1,12 @@
-print("Multiplikationstabelle ( kleines 1-mal-1")
-Zahl = int(input(" Bitte gib eine zhal ein:"))
-for i in range(1,11):
-    ergebnis = Zahl * i
-    print(f"{Zahl} * {i} = {ergebnis}")
+print("Multiplikationstabelle ( kleines 1-mal-1 )")
+while True :
+    try :
+     zahl = int(input(" Bitte gib eine Zahl ein: "))
+     for i in range(1,11) :
+         if range :
+             ergebnis = zahl * i
+             print(f"{zahl} * {i} = {ergebnis}")
+         else :
+             break
+    except ValueError:
+             print("Fehler: Bitte gib eine gültige Zahl ein!")
