68dca99e0627bbbd2468c9b8e0220667c8994aca
kl <kl@intentus.de>
2024-10-10 10:37:37 +0200

    Verbesserung Aufgabe 6
    - Prompt Gestaltung
    - Korrektur der Eingabe ermöglicht (Meldung)
    - ohne while true und break
    - Rechtschreibung

--- Aufgabe 6.py
+++ Aufgabe 6.py
@@ -1,13 +1,21 @@
+print("Willkommen zum Zahlenraten")
 import random
-print("Willkommen zum Zahlen raten!")
-print("Gesucht wird eine Zahl zwischen 1 und 100.")
-Zahl = random.randint(1,100)
-while True:
-    versuch = int(input("Rate meine Zahl:"))
-    if versuch == Zahl:
-        print("Erraten!")
-        break
-    if versuch < Zahl : print("zu klein!")
-    if versuch > Zahl : print("zu groß!")
-
-
+Zufallszahl = random.randint(1,100)
+geraten = False
+while not geraten:
+    try:
+        tipp = int(input("Rate eine Zahl zwischen 1 und 100: "))
+        if tipp < 1 or tipp > 100:
+            print("Die Zahl muss zwischen 1 und 100 liegen.")
+            continue
+        if tipp < Zufallszahl :
+            print("Zu niedrig!")
+            continue
+        elif tipp > Zufallszahl :
+            print("Zu hoch!")
+            continue
+        else:
+            print("Glückwunsch! Du hast die Zahl erraten.")
+        geraten = True
+    except ValueError :
+        print("Bitte gib eine Zahl zwischen 1 und 100 ein!")
