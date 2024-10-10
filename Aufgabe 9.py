ad1e6545ead5baee9297864a88545074a925105b
kl <kl@intentus.de>
2024-10-10 15:04:01 +0200

    Verbesserung Aufgabe 9
    - Code angepasst
    - Ausgabe, die in jedem Fall die Zahl und in den Testfällen zusätzlich das “Ping/Pong/PingPong” ausgibt

--- Aufgabe 9.py
+++ Aufgabe 9.py
@@ -1,15 +1,10 @@
-Zahl = int()
-for Zahl in range(1,101):
-    if Zahl % 3 == 0 and Zahl % 5 == 0:
-        print("PingPong")
-    elif Zahl % 3 == 0:
-        print("Ping")
-    elif Zahl % 5 == 0:
-        print("Pong")
-    else:
-        print(Zahl)
-if Zahl > 100:
-    print ("ungültige Zahl")
-elif Zahl < 1:
-    print("ungültige Zeit")
-    
+print("PingPong!")
+for i in range(1, 101):
+    output = str(i)
+    if i % 3 == 0 and i % 5 == 0:
+        output += " PingPong"
+    elif i % 3 == 0:
+        output += " Ping"
+    elif i % 5 == 0:
+        output += " Pong"
+    print(output)
