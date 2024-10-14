77d474ca0c770079133a472e66b678828c1942fe
kl <kl@intentus.de>
2024-10-14 08:48:00 +0200

    Verbesserung Aufgabe 6
    - Gestaltung

--- Aufgabe 6.py
+++ Aufgabe 6.py
@@ -1,21 +1,27 @@
-print("Willkommen zum Zahlenraten")
 import random
-Zufallszahl = random.randint(1,100)
-geraten = False
-while not geraten:
-    try:
-        tipp = int(input("Rate eine Zahl zwischen 1 und 100: "))
-        if tipp < 1 or tipp > 100:
-            print("Die Zahl muss zwischen 1 und 100 liegen.")
-            continue
-        if tipp < Zufallszahl :
-            print("Zu niedrig!")
-            continue
-        elif tipp > Zufallszahl :
-            print("Zu hoch!")
-            continue
-        else:
-            print("Glückwunsch! Du hast die Zahl erraten.")
-        geraten = True
-    except ValueError :
-        print("Bitte gib eine Zahl zwischen 1 und 100 ein!")
+
+def zahlen_raten():
+    zufallszahl = random.randint(1, 100)
+    geraten = False
+
+    print("Ich habe eine Zahl zwischen 1 und 100 gewählt. Versuche, sie zu erraten!")
+
+    while nicht_geraten(geraten):
+        try:
+            rateversuch = int(input("Gib deine Schätzung ein: "))
+
+            if rateversuch < 1 or rateversuch > 100:
+                print("Deine Zahl liegt außerhalb des gültigen Bereichs (1-100).")
+            elif rateversuch < zufallszahl:
+                print("Zu niedrig!")
+            elif rateversuch > zufallszahl:
+                print("Zu hoch!")
+            else:
+                print("Glückwunsch! Du hast die richtige Zahl erraten.")
+                geraten = True
+        except ValueError:
+            print("Bitte gib eine gültige Zahl ein.")
+
+
+def nicht_geraten(geraten):
+    return not geraten
