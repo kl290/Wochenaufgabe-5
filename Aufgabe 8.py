228c4e93011c2484bb22b44371e8dc14738e5678
kl <kl@intentus.de>
2024-10-14 15:06:38 +0200

    Aufgabe 8 (verbessert)

--- Aufgabe 8.py
+++ Aufgabe 8.py
@@ -1,5 +1,20 @@
 print("Durchschnittsberechner")
-Zahl = input("Bitte gib die Zahlen ein, getrennt durch Leerzeichen:")
-Zahlenliste = list(map(float, Zahl.split()))
-durchschnitt = sum(Zahlenliste) / len(Zahlenliste)
-print(f"Der Durchschnitt der eingegebenen Zahlen ist: {durchschnitt}")
+
+while True:
+    try:
+        eingabe = input("Bitte gib die Zahlen ein, getrennt durch Leerzeichen: ")
+
+        zahlen_liste = list(map(float, eingabe.split()))
+
+        if not zahlen_liste:
+            print("Es wurden keine Zahlen eingegeben. Bitte versuche es erneut.")
+            continue
+
+        durchschnitt = sum(zahlen_liste) / len(zahlen_liste)
+
+        print(f"Der Durchschnitt der eingegebenen Zahlen ist: {durchschnitt}")
+        break
+
+    except ValueError:
+        print("Fehler: Du hast einen Buchstaben oder ungültige Zeichen eingegeben. Bitte gib nur Zahlen ein, ", end= "")
+        print("getrennt durch Leerzeichen.")
