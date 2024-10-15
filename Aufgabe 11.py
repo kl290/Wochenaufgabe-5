51c262b2599fd733344380d5417b3b4857268a60
kl <kl@intentus.de>
2024-10-15 15:52:31 +0200

    Aufgabe 11
    - an Feedback angepasst

--- Aufgabe 11.py
+++ Aufgabe 11.py
@@ -1,12 +1,15 @@
+def eingabe_zahlen_liste():
+    while True:
+        try:
+            eingabe = input("Bitte eine Liste von Zahlen eingeben (durch Kommas getrennt): ")
+            return list(map(float, eingabe.split(',')))
+        except ValueError:
+            print("Fehler: Bitte nur gültige Zahlen eingeben.")
+
+
 def groesste_zahl(liste):
     return max(liste)
 
 
-while True:
-    try:
-        eingabe = input("Bitte eine Liste von Zahlen eingeben (durch Kommas getrennt): ")
-        zahlen_liste = [float(x) for x in eingabe.split(',')]
-        print(f"Die größte Zahl in der Liste ist: {groesste_zahl(zahlen_liste)}")
-        break
-    except ValueError:
-        print("Fehler: Bitte nur gültige Zahlen eingeben.")
+zahlen_liste = eingabe_zahlen_liste()
+print(f"Die größte Zahl in der Liste ist: {groesste_zahl(zahlen_liste)}")
