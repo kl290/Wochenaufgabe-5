1b3e27b6a7572587d3361e0fd4cf89196af2fa21
kl <kl@intentus.de>
2024-10-16 09:15:49 +0200

    Aufgabe 5
    - if/elif Block angepasst
    -"Die Note lautet"-Text angepasst
    - Code formatiert

--- Aufgabe 5.py
+++ Aufgabe 5.py
@@ -4,24 +4,21 @@ def punkt_eingabe():
             zahl = int(input("Geben Sie eine Punktezahl zwischen 0 und 100 ein: "))
             if 0 <= zahl <= 100:
                 return zahl
-            else:
-                print("Bitte gib eine Zahl zwischen 0 und 100 ein.")
         except ValueError:
             print("Bitte gib eine gültige Zahl ein.")
 
 
 punkte = punkt_eingabe()
 
-if   punkte > 89:
-    print("Die Note lautet: Sehr gut")
-elif punkte > 79:
-    print("Die Note lautet: Gut")
-elif punkte > 69:
-    print("Die Note lautet: Befriedigend")
-elif punkte > 59:
-    print("Die Note lautet: Ausreichend")
-elif punkte < 60:
-    print("Die Note lautet: Nicht bestanden")
+n = "Die Note lautet:"
 
-
-
+if punkte >= 89:
+    print(f"{n} Sehr gut")
+elif punkte >= 79:
+    print(f"{n} Gut")
+elif punkte >= 69:
+    print(f"{n} Befriedigend")
+elif punkte >= 59:
+    print(f"{n} Ausreichend")
+else:
+    print(f"{n} Nicht bestanden")
