1fcab4770e7424ce760ba26e182f81ab2838d6c5
kl <kl@intentus.de>
2024-10-16 10:43:45 +0200

    Aufgabe 11
    - Codeblock minimiert
    - Zeile 5 vereinfacht

--- Aufgabe 11.py
+++ Aufgabe 11.py
@@ -1,15 +1,11 @@
-def eingabe_zahlen_liste():
+def groesste_zahl():
     while True:
         try:
             eingabe = input("Bitte eine Liste von Zahlen eingeben (durch Kommas getrennt): ")
-            return list(map(float, eingabe.split(',')))
+            zahlen_liste = list(map(float, eingabe.split(',')))
+            return max(zahlen_liste)
         except ValueError:
             print("Fehler: Bitte nur gültige Zahlen eingeben.")
 
 
-def groesste_zahl(liste):
-    return max(liste)
-
-
-zahlen_liste = eingabe_zahlen_liste()
-print(f"Die größte Zahl in der Liste ist: {groesste_zahl(zahlen_liste)}")
+print(f"Die größte Zahl in der Liste ist: {groesste_zahl()}")
