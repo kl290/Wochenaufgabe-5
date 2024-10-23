0118e32a8c9fa5b29f9dbc5e0c18f86a38e38eff
kl <kl@intentus.de>
2024-10-23 15:27:22 +0200

    Aufgabe 14

--- Aufgabe14.py
+++ Aufgabe14.py
@@ -15,7 +15,7 @@ def woerter_zaehlen():
             print(f"Der Satz enthält {anzahl_zeichen} Zeichen.")
             print(f"Der Satz enthält {anzahl_buchstaben} Buchstaben.")
             print(f"Der Satz enthält {anzahl_ziffern} Ziffern.")
-            print(f"Der Satz enthält {anzahl_whitespaces} Whites  paces.")
+            print(f"Der Satz enthält {anzahl_whitespaces} Whitespaces.")
             break
         else:
             print("Fehler: Sie haben keinen gültigen Satz eingegeben. Bitte erneut versuchen.")
