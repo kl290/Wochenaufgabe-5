c76830706e5f873ea4c6045033a807e96b002fa9
kl <kl@intentus.de>
2024-10-23 15:27:06 +0200

    Aufgabe 14

---
+++ Aufgabe14.py
@@ -0,0 +1,24 @@
+def woerter_zaehlen():
+    while True:
+        satz = input("Bitte einen Satz eingeben: ").strip()
+
+        if satz:
+            woerter = satz.split()
+            anzahl_woerter = len(woerter)
+            anzahl_zeichen = len(satz)
+            anzahl_buchstaben = sum(c.isalpha() for c in satz)
+            anzahl_ziffern = sum(c.isdigit() for c in satz)
+            anzahl_whitespaces = sum(c.isspace() for c in satz)
+
+            print(f"\nStatistik:")
+            print(f"Der Satz enthält {anzahl_woerter} Wörter.")
+            print(f"Der Satz enthält {anzahl_zeichen} Zeichen.")
+            print(f"Der Satz enthält {anzahl_buchstaben} Buchstaben.")
+            print(f"Der Satz enthält {anzahl_ziffern} Ziffern.")
+            print(f"Der Satz enthält {anzahl_whitespaces} Whites  paces.")
+            break
+        else:
+            print("Fehler: Sie haben keinen gültigen Satz eingegeben. Bitte erneut versuchen.")
+
+
+woerter_zaehlen()
