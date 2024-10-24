65d9048de1e8e64314c5318dd58de1d7ab29331d
kl <kl@intentus.de>
2024-10-24 15:39:06 +0200

    Aufgabe 11
    - Aufteilung Programm

--- Aufgabe11.py
+++ Aufgabe11.py
@@ -1,13 +1,15 @@
 def groesste_zahl():
-    while True:
-        eingabe = input("Bitte eine Liste von Zahlen eingeben (durch Kommas getrennt): ")
-        try:
-            zahlen_liste = list(map(float, eingabe.split(',')))
-        except ValueError:
-            print("Fehler: Bitte nur gültige Zahlen eingeben.")
-            continue
+    eingabe = input("Bitte eine Liste von Zahlen eingeben (durch Kommas getrennt): ").strip()
 
-        return max(zahlen_liste)
+    try:
+        zahlen_liste = list(map(float, eingabe.split(',')))
+    except ValueError:
+        print("Fehler: Bitte nur gültige Zahlen im richtigen Format eingeben.")
+        return groesste_zahl()
+
+    groesste = max(zahlen_liste)
+
+    print(f"Die größte Zahl in der Liste ist: {groesste}")
 
 
-print(f"Die größte Zahl in der Liste ist: {groesste_zahl()}")
+groesste_zahl()
