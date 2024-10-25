45fabf2dffee0dea0171a1aa19ff146e772748c9
kl <kl@intentus.de>
2024-10-25 11:00:33 +0200

    Aufgabe 13 - EVA Prinzip - Doppelung entfernt

--- Aufgabe13.py
+++ Aufgabe13.py
@@ -1,11 +1,12 @@
 def woerter_zaehlen():
     satz = input("Bitte einen Satz eingeben: ").strip()
 
-    while not satz:
+    if not satz:
         print("Fehler: Sie haben keinen gültigen Satz eingegeben. Bitte erneut versuchen.")
-        satz = input("Bitte einen Satz eingeben: ").strip()
-
+        return woerter_zaehlen()
     woerter = satz.split()
+
     print(f"Der Satz enthält {len(woerter)} Wörter.")
+
 
 woerter_zaehlen()
