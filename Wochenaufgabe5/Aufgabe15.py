ccdacfb35491003f77804dee122b91cc572236c7
kl <kl@intentus.de>
2024-11-05 08:58:19 +0100

    Aufgabe 15 - Zahlen/Ziffern Eingabe ermöglicht
    - zählt gleiche Worte egal ob Groß- oder Kleinschreibung
    
    - Probleme mit Interpunktion behoben

---
+++ Wochenaufgabe5/Aufgabe15.py
@@ -0,0 +1,22 @@
+def woerter_zaehlen():
+    satz = input("Bitte einen Text eingeben: ")
+    anzahl_zahlen = sum(1 for wort in satz.split() if wort.isdigit())
+    woerter = satz.split()
+    haeufigkeit = {}
+
+    for wort in woerter:
+
+        wort = wort.strip(".,!?;:()\"")
+
+        if wort:
+            wort = wort.lower()
+            if wort in haeufigkeit:
+                haeufigkeit[wort] += 1
+            else:
+                haeufigkeit[wort] = 1
+
+    print(f"Der Text enthält {len(woerter)} Wörter, und {anzahl_zahlen} Zahlen insgesamt.")
+    print("Häufigkeit der Wörter und Zahlen:", haeufigkeit)
+
+
+woerter_zaehlen()
