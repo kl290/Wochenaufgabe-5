3c2cd098a5fac1e6f3a1095813014e5282d0cb23
kl <kl@intentus.de>
2024-12-13 14:26:04 +0100

    Aufgabe 15

--- Aufgabe15.py
+++ Aufgabe15.py
@@ -1,22 +1,19 @@
 def woerter_zaehlen():
     satz = input("Bitte einen Text eingeben: ")
+
+
     anzahl_zahlen = sum(1 for wort in satz.split() if wort.isdigit())
     woerter = satz.split()
     haeufigkeit = {}
 
     for wort in woerter:
-
         wort = wort.strip(".,!?;:()\"")
-
         if wort:
             wort = wort.lower()
-            if wort in haeufigkeit:
-                haeufigkeit[wort] += 1
-            else:
-                haeufigkeit[wort] = 1
+            haeufigkeit[wort] = haeufigkeit.get(wort, 0) + 1
+
 
     print(f"Der Text enthält {len(woerter)} Wörter, und {anzahl_zahlen} Zahlen insgesamt.")
-    print("Häufigkeit der Wörter und Zahlen:", haeufigkeit)
+    print("Häufigkeit der Wörter:", haeufigkeit)
 
-
-(woerter_zaehlen())
+woerter_zaehlen()
